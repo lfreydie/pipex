974431e03d855d3bd814824460578efbaa258270
lefreydier <lefreydier@LAPTOP-U4OF5VH2>
2023-04-11 11:32:17 +0200

    homework

--- includes/pipex.h
+++ includes/pipex.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lfreydie <lfreydie@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lefreydier <lefreydier@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 13:06:33 by lfreydie          #+#    #+#             */
-/*   Updated: 2023/04/06 15:28:14 by lfreydie         ###   ########.fr       */
+/*   Updated: 2023/04/10 19:17:25 by lefreydier       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,40 @@
 # include <stdlib.h>
 # include <stdio.h>
 # include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
 
 # define ERROR 1
 # define SUCCESS 0
 # define ERR "Error\n"
 # define ERR_ARG "Invalid number of arguments\n"
+# define ERR_CMD "Command not found\n"
+# define ERR_PIP "Pipe didn't work\n"
 # define ERR_ENV "Environnement error\n"
 # define ERR_NOP "The file doesn't open or isn't a file\n"
+
+typedef struct s_pipex
+{
+	pid_t	child_1;
+	pid_t	child_2;
+	int		tube[2];
+	int		infile;
+	int		outfile;
+	char	*cmd;
+	char	**cmd_args;
+	char	**path;
+	char	**env;
+}	t_pipex;
+
+//	PARSE
+t_pipex	ft_parse(int narg, char *arg, char **env);
+
+//	PIPEX
+void	pipex(int f1, int f2, char **av, char **env);
+void	child_1_process(int f1, char *cmd);
+
+//	EXIT
+void	ft_exit(char *err, int fd);
 
 #endif
--- src/parse.c
+++ src/parse.c
@@ -1,27 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   parse.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lefreydier <lefreydier@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/06 13:12:31 by lfreydie          #+#    #+#             */
-/*   Updated: 2023/04/10 19:04:19 by lefreydier       ###   ########.fr       */
+/*   Created: 2023/04/10 19:12:16 by lefreydier        #+#    #+#             */
+/*   Updated: 2023/04/10 19:26:34 by lefreydier       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
-int	main(int ac, char **av, char **env)
+t_pipex	ft_parse(int narg, char *arg, char **env)
 {
 	t_pipex	pipex;
 
-	if (!env | !env[0])
-		return (ft_exit(ERR_ENV, 2));
-	pipex = ft_parse(ac - 1, av + 1, env);
-	pipex.infile = open(av[1], O_RDONLY);
-	pipex.outfile = open(av[1], O_CREAT | O_RDWR | O_TRUNC, 0744);
-	if (f1 < 0 || f2 < 0)
-		return (ft_exit(ERR_NOP, 2));
-	pipex(f1, f2, av, env);
+	if (narg != 4)
+		return (ft_exit(ERR_ARG, 2));
+	pipex = ft_calloc()
+	return (pipex);
 }
